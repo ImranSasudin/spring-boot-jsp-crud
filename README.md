641d09b0361cce64b2d8bc693ca2bd5fcf88091f
ImranSasudin <imransasudin@gmail.com>
2020-07-08 22:04:39 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# springboot-jsp-crud
+
+Based on RESTAPI below, i changed it into JSP as front-end<br>
+https://github.com/ImranSasudin/spring-boot-restapi-crud
---
+++ src/main/java/com/example/springjsp/SpringbootJspCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJspCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJspCrudApplication.class, args);
+	}
+
+}
